715d404e035b1c3eb75d143bffa106c6ea5199d5
JoaoCelinoGualberto <joaocg14@gmail.com>
2024-02-15 00:34:20 -0300

    chore: improving section 1 styles

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Section1Container, ContentContainer, Content, Image, Title, Paragraph, Button } from '../styles/components/Section1Styles';
 import sampleImage from '../assets/sample-image-sec1.jpg'; 
+import { MdArrowOutward } from "react-icons/md";
 
 const Section1 = () => {
   return (
     <Section1Container>
       <ContentContainer>
         <Content>
-          <Title>APOIE O PROJETO RESGATE</Title>
+          <Title>Apoie o projeto Resgate</Title>
           <Paragraph>
             Junte-se a nós para construir um refúgio para aqueles que precisam! Vamos mudar vidas, transformar histórias e restaurar famílias com fé, foco e determinação. O seu apoio fará a diferença na vida de dependentes químicos e mulheres em situação de vulnerabilidade, através da nossa vida Deus vai resgatar milhões de outras vidas.
           </Paragraph>
-          <Button>Ajude</Button>
+          <Button>QUERO FAZER PARTE  <i><MdArrowOutward /></i></Button>
         </Content>
         <Image src={sampleImage} alt="Sample Image" />
       </ContentContainer>
--- src/styles/components/Section1Styles.js
+++ src/styles/components/Section1Styles.js
@@ -1,10 +1,10 @@
-// styles/components/Section1Styles.js
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Section1Container = styled.section`
-  background-color: #f0f0f0;
+  background: linear-gradient(to bottom, #ffffff, #ffccbc);
   padding: 40px;
   text-align: center;
+  padding: 1% 10%; 
 `;
 
 export const ContentContainer = styled.div`
@@ -13,32 +13,39 @@ export const ContentContainer = styled.div`
 `;
 
 export const Content = styled.div`
-  width: 50%; /* Ocupa metade da largura da seção */
-  text-align: left; /* Alinha o conteúdo à esquerda */
+  width: 50%;
+  text-align: left;
 `;
 
 export const Image = styled.img`
   width: 50%;
   height: auto;
-  border
+  margin-left: 4%;
+  border-radius: 15px 50px 15px 50px; 
 `;
+
 
 export const Title = styled.h2`
   font-size: 36px;
   margin-bottom: 20px;
+  font-weight: 500;
 `;
 
 export const Paragraph = styled.p`
   font-size: 18px;
   margin-bottom: 20px;
+  margin-right: 6%;
+  text-align: justify;
 `;
 
 export const Button = styled.button`
-  background-color: #333;
+  background-color: #4CAF50;
   color: #fff;
   padding: 10px 20px;
   border: none;
   cursor: pointer;
   font-size: 16px;
-  border-radius: 5px;
+  border-radius: 20px;
+  display: flex;
+  align-items: center; 
 `;
