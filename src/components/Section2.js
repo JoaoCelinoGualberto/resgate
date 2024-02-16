5f1fb5f354610697633e2b51c6ed38ba5bcdb8b9
JoaoCelinoGualberto <joaocg14@gmail.com>
2024-02-16 05:04:30 -0300

    chore: section 2 responsive version

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,47 +1,21 @@
-// components/Section2.js
+// Componente Section2.js
 import React from 'react';
-import { Section2Container, Title, List, ListItem, ListItemContent } from '../styles/components/Section2Styles';
-import { FaRegSmile } from 'react-icons/fa'; // Importa o ícone da biblioteca React Icons
+import { Section2Container, Title, List, Card, ListItem, Icon, iconsMap } from '../styles/components/Section2Styles';
 
 const Section2 = () => {
   return (
     <Section2Container>
       <Title>VAMOS PROPORCIONAR</Title>
       <List>
-        <ListItem>
-          <ListItemContent>
-            Identidade
-            <FaRegSmile /> {/* Ícone da biblioteca React Icons */}
-          </ListItemContent>
-        </ListItem>
-        <ListItem>
-          <ListItemContent>
-            Dignidade
-            <FaRegSmile /> {/* Ícone da biblioteca React Icons */}
-          </ListItemContent>
-        </ListItem>
-        <ListItem>
-          <ListItemContent>
-            Autoestima
-            <FaRegSmile /> {/* Ícone da biblioteca React Icons */}
-          </ListItemContent>
-        </ListItem>
-        <ListItem>
-          <ListItemContent>
-            Esperança
-            <FaRegSmile /> {/* Ícone da biblioteca React Icons */}
-          </ListItemContent>
-        </ListItem>
-        <ListItem>
-          <ListItemContent>
-            Desenvolvimento pessoal
-            <FaRegSmile /> {/* Ícone da biblioteca React Icons */}
-          </ListItemContent>
-        </ListItem>
+        {Object.keys(iconsMap).map((item, index) => (
+          <Card key={index}>
+            <Icon>{React.createElement(iconsMap[item])}</Icon>
+            <ListItem>{item}</ListItem>
+          </Card>
+        ))}
       </List>
     </Section2Container>
   );
 }
 
 export default Section2;
-
--- src/styles/components/Section2Styles.js
+++ src/styles/components/Section2Styles.js
@@ -1,16 +1,18 @@
 // styles/components/Section2Styles.js
 import styled from 'styled-components';
+import { FaRegUser, FaRegHeart, FaRegSmile, FaRegStar } from 'react-icons/fa'; 
+import { GiBrain } from "react-icons/gi";
 
 export const Section2Container = styled.section`
-  background-color: #000; 
-  padding: 40px;
+  background-color: #fff; 
+  color: #000;
+  padding: 3% 5%; /* Reduzindo o padding para telas menores */
   text-align: center;
-  padding: 1% 10%; 
 `;
 
 export const Title = styled.h2`
   font-size: 23px; 
-  margin-bottom: 30px; 
+  margin-bottom: 20px; 
   font-weight: 500;
 `;
 
@@ -18,41 +20,46 @@ export const List = styled.ul`
   list-style: none;
   padding: 0;
   display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
+  justify-content: center; /* Centralizando os cards */
+  flex-wrap: wrap; /* Permitindo que os cards quebrem para a próxima linha */
+  gap: 20px; /* Espaçamento entre os cards */
+`;
+
+export const Card = styled.div`
+  background-color: #fff;
+  border: 3px solid #ffccbc; /* Adiciona a borda */
+  width: 150px; /* Largura do círculo */
+  height: 150px; /* Altura do círculo */
+  border-radius: 50%; /* Transforma em um círculo */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 1; /* Para ficar acima da linha */
+`;
+
+export const Icon = styled.div`
+  font-size: 40px;
+  margin-bottom: 10px;
+  color: #ffccbc;
+  &:hover {
+    transform: scale(1.19); 
+  }
 `;
 
 export const ListItem = styled.li`
-  font-size: 20px; 
-  margin-bottom: 20px; 
-  width: calc(20% - 17px); 
-  background-color: #FFCCBC; /* Cor de fundo */
-  padding: 20px;
-  border-radius: 20px; /* Borda arredondada */
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Sombra */
-  transition: transform 0.3s ease; /* Transição suave do tamanho */
-  overflow: hidden; /* Oculta conteúdo fora da borda inclinada */
-  
-  &:hover {
-    transform: translateY(-10px); /* Eleva o card no hover */
-  }
-  
-  &::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: #FF7043; /* Cor da borda inclinada */
-    transform-origin: bottom right;
-    transform: skewX(-15deg); /* Inclinação para a esquerda */
-    z-index: -1; /* Coloca a borda atrás do conteúdo */
-  }
+  font-size: 0.76em; /* 1em é equivalente ao tamanho da fonte do elemento pai */
+  color: #000;
+  text-align: center; /* Alinha o texto ao centro horizontalmente */
+  margin-top: 10px; /* Espaçamento acima do título */
+  text-transform: uppercase; 
 `;
 
-export const ListItemContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+// Mapeamento de ícones para cada item
+export const iconsMap = {
+  Identidade: FaRegUser,
+  Dignidade: FaRegHeart,
+  Autoestima: FaRegSmile,
+  Esperança: FaRegStar,
+  'Desenvolvimento pessoal': GiBrain,
+};
