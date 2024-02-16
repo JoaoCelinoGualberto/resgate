89f8680a1bdb3de0e3774cd5f0f6075bece53af5
JoaoCelinoGualberto <joaocg14@gmail.com>
2024-02-16 08:48:37 -0300

    chore: sections update

--- src/components/Section5.js
+++ src/components/Section5.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import { Section5Container, Title, Paragraph, Button } from '../styles/components/Section5Styles';
+import React from "react";
+import {
+  Section5Container,
+  Paragraph,
+  Button,
+  TextContainer,
+} from "../styles/components/Section5Styles";
 
 const Section5 = () => {
   return (
     <Section5Container>
-      <Paragraph>Missão se faz com os joelhos de quem ora, com os pés de quem vai e com o dinheiro de quem investe.</Paragraph>
-      <Button>Salve Vidas</Button>
+      <TextContainer>
+        <Paragraph>
+          Missão se faz com os joelhos de quem ora, com os pés de quem vai e com
+          o dinheiro de quem investe.
+        </Paragraph>
+      </TextContainer>
+      <Button>SALVE VIDAS</Button>
     </Section5Container>
   );
-}
+};
 
 export default Section5;
